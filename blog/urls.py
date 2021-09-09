06a319ace2a2eb99da3a1096b11ebc75effa71d5
amit <amit.3k3@gmail.com>
2021-09-09 23:28:38 +0530

    Almost Complete

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name = 'welcomePage'),
     path('posts', views.posts, name = 'postsPage'),
-    path('posts/<slug:slug>', views.postDetail, name = 'postDetailsPage')
+    path('posts/<slug:postTitle>', views.postDetail, name = 'postDetailsPage')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,48 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 
-def welcome(request):
-    return render(request, 'blog/welcome.html')
+post1Detail = {
+    'title':    'qw-we-er',
+    'details':  'sdjkhfsduk syfsfsopf oshfow yhfwhrif wiusyhithds'
+}
 
-def posts(request, posts):
-    return render(request, 'blog/posts.html', {
-        'posts': posts
+post2Detail = {
+    'title':    'as-sd-df',
+    'details':  's;lkgp dipgdropi rt0w9sejfjwdfwe iu0w8spdokf we-38'
+}
+
+post3Detail = {
+    'title':    'zx-xc-cv',
+    'details':  'sdlkfj oeuroqwe[po oe9ru[opweruw39r8 lcaskcmosd'
+}
+
+allPosts    =   [post1Detail, post2Detail, post3Detail]
+allTitles    =   [post1Detail['title'], post2Detail['title'], post3Detail['title']]
+
+
+def welcome(request):
+    return render(request, 'blog/welcome.html', {
+        'allTitles': allTitles
     })
 
-def postDetail(request, postdetail):
-    return render(request, 'blog/postdetail.html', {
-        'postDetail': postDetail
+def posts(request):
+    return render(request, 'blog/posts.html', {
+        'allPosts': allPosts
+    })
+
+def postDetail(request, postTitle):
+    toekn = False
+    for post in allPosts:
+        if post['title'] == postTitle:
+            toekn = True
+            break
+        else:
+            print('Post not found')
+            token = False
+    print(post)
+    return render(request, 'blog/postdetails.html', {
+        'post'      :   post,
+        'postTitle' :   postTitle
     })
